205fee2903bde894deb4f44e36af7237c6d2f6e6
David Silveira <jdsilveira@gmail.com>
2013-09-27 02:07:07 -0300

    cosmetic fixes

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 $().ready(function(){
 
-  var backgrounds = ['/img/back-1.jpg', '/img/back-2.jpg','/img/scarface-red.jpg','/img/jamiroquai.jpg'];
+  var backgrounds = ['/img/music-equalizer.jpg', '/img/music-record-player.jpg', '/img/back-1.jpg', '/img/back-2.jpg','/img/scarface-red.jpg','/img/jamiroquai.jpg','/img/preparado-surfer.jpg'];
 
   var background = backgrounds[Math.floor(Math.random()*backgrounds.length)]
 
