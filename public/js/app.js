541cb203b23d24f5f65b9e98abcd2e3262bbabd3
David Silveira <jdsilveira@gmail.com>
2015-06-14 03:11:46 -0300

    Remove useless files

--- public/js/app.js
+++
@@ -1,9 +0,0 @@
-$().ready(function(){
-
-  var backgrounds = ['/img/music-equalizer.jpg', '/img/music-record-player.jpg', '/img/back-1.jpg', '/img/back-2.jpg','/img/scarface-red.jpg','/img/jamiroquai.jpg','/img/preparado-surfer.jpg'];
-
-  var background = backgrounds[Math.floor(Math.random()*backgrounds.length)]
-
-  $(document.body).css('background-image', 'url('+background+')');
-
-});
